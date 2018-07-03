332c4b1bb23bd68960ef112728bc3e9be27a7e24
Isaiah Harris <harrisfambam@gmail.com>
2018-07-03 09:28:41 -1000

    complete secret number generator

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,13 @@
 'use strict';
 module.exports = function() {
-  
-};
+    var rand = Math.floor(Math.random()* 100001);
+     function random(){
+            return rand;
+     }
+    
+    
+    return random
+
+   
+}
+      
